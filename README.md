767ac51ddefa96f8297959b4dfa49646426bb6d4
Hugo Hromic <hugo.hromic@deri.org>
2014-11-16 00:03:32 +0000

    Initial commit of the program

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 gpstracker-node
 ===============
 
-Simple Live GPS Tracker for Node.JS
+A simple Live GPS Tracker for Node.JS for [OsmAnd](https://play.google.com/store/apps/details?id=net.osmand) clients.
+
+Module Requirements
+-------------------
+
+This program requires the following modules to be installed:
+
+```shell
+npm install commander
+npm install express
+npm install log4js
+npm install websocket
+```
+
+Usage
+-----
+
+This simple program tracks a collection of OsmAnd GPS clients and allows for real-time live display of them into a Google Maps interactive display. The program accepts two optional arguments:
+
+```shell
+$ node gpstracker.js <portNumber> <clientsConfig>
+```
+
+* ```portNumber``` is the http port to use for both static and websocket connections.
+* ```clientsConfig``` is the GPS clients meta-data configuration file in JSON format.
+
+**Example:**
+
+```shell
+$ node gpstracker.js -p 2227
+[2014-11-15 23:57:01.077] [INFO] gpstracker - server listening at http://0.0.0.0:2227
+```
+
+Then you start your OsmAnd clients and allow for some updates to be sent via the Internet first. Afterwards you point your favourite HTML5 browser to the server address above and you can watch a real-time live display of all your tracked clients. You can click on any client marker in the map to see detailed tracking data such as position, speed and HDOP.
+
+GPS Clients Configuration
+-------------------------
+
+OsmAnd Configuration
+--------------------
---
+++ gpstracker.js
@@ -0,0 +1,65 @@
+/**
+ * Live GPS Tracker server.
+ *
+ * Hugo Hromic - http://github.com/hhromic
+ * Apache 2.0 license
+ */
+
+// Required modules
+var program = require('commander');
+var log4js = require('log4js');
+var express = require('express');
+var websocket = require('websocket');
+
+// Loggers
+var logger = log4js.getLogger('gpstracker');
+var updLogger = log4js.getLogger('update');
+var wsLogger = log4js.getLogger('websocket');
+
+// Setup and parse program arguments
+program.version('1.0.0');
+program.option('-p, --port <port>', 'server port [2223]', Number, 2223);
+program.option('-c, --clients <json_file>', 'GPS clients data [clients.json]', 'clients.json');
+program.parse(process.argv);
+
+// Load GPS clients data and initialise lastUpd
+var clients = require('./' + program.clients);
+var lastUpd = {};
+
+// Express App
+var app = express();
+app.enable('trust proxy');
+
+// App server
+var server = app.listen(program.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  logger.info('server listening at http://%s:%s', host, port);
+});
+
+// Websocket server
+var wsServer = new websocket.server({httpServer: server});
+wsServer.on('request', function (request) {
+    var connection = request.accept(null, request.origin);
+    connection.on('close', function (reasonCode, description) {
+        wsLogger.debug('%s disconnected', connection.remoteAddress);
+    });
+    connection.send(JSON.stringify({lastUpd: lastUpd}));
+    wsLogger.debug('%s connected - protocol version %d',
+        connection.remoteAddress, connection.webSocketVersion);
+});
+
+// GPS client update resource and static content
+app.get('/update', function (request, response) {
+  var cid = request.query.cid;
+  if (cid !== undefined && cid in clients) {
+      lastUpd[cid] = {client: clients[cid], data: request.query};
+      wsServer.broadcast(JSON.stringify(lastUpd[cid]));
+      response.send('ok\n');
+      updLogger.debug('%s sent %j', request.ip, request.query);  
+  } else {
+      response.send('invalid\n');
+      updLogger.warn('invalid client \'%s\' from %s', cid, request.ip);
+  }
+});
+app.use(express.static(__dirname + '/public'));
